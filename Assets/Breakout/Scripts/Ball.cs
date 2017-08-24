36349ad73494f18f16a42b96909e8f803719dcc0
bananabiz <lee21acc973@163.com>
2017-08-24 12:10:29 +1000

    fix fire

---
+++ Assets/Breakout/Scripts/Ball.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Breakout
+{ 
+public class Ball : MonoBehaviour {
+
+        public float speed = 5f; //Speed that the ball travels
+
+        private Vector3 velocity; //Velocity of the ball (Direction X Speed)
+
+        //Sends the ball flying in a given direction
+        public void Fire(Vector3 direction)
+        {
+        velocity = direction* speed;
+            //Vector3.Distance()
+        }
+
+        void OnCollisionEnter2D(Collision2D other)
+        {
+            //Grab the contact point of the collision
+            ContactPoint2D contact = other.contacts[0];
+            //Calculate the reflect vector
+            Vector3 reflect = Vector3.Reflect(velocity, contact.normal);
+            //Calculating new veloctiy (replacing the direction but not speed)
+            velocity = reflect.normalized * velocity.magnitude;
+        }
+
+
+	    // Use this for initialization
+	    void Start () {
+	    	
+	    }
+	    
+	    // Update is called once per frame
+	    void Update ()
+        {
+            //Move ball using velocity and deltaTime
+            transform.position += velocity * Time.deltaTime;
+	    }
+    }
+}
