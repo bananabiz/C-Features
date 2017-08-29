f6316ae964d5451761e14dabb77598bdc023c259
bananabiz <lee21acc973@163.com>
2017-08-29 12:17:02 +1000

    Added changes

---
+++ Assets/Breakout/Scripts/ScoreManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace Breakout
+{
+public class ScoreManager : MonoBehaviour
+    {
+        public int score;
+        public Text scoreText;
+
+        // Use this for initialization
+        void Start()
+        {
+            scoreText = this.GetComponent<Text>();
+        }
+
+	    void Update ()
+        {
+            if (score < 10)
+            {
+                scoreText.text = "Score: " + score.ToString();
+            }
+            else if (score == 10)
+            {
+                scoreText.text = "STAGE CLEAR!";
+                SceneManager.LoadScene(0);
+            }
+	    }
+    }
+}
