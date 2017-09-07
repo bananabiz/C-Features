b6beaca6050e993bc482c9bd85ae2502c03ea1b9
bananabiz <lee21acc973@163.com>
2017-09-07 15:54:28 +1000

    updated MineSweeper2D (add raycast reveal)

--- Assets/~MineSweeper2D/Scripts/Grid.cs
+++ Assets/~MineSweeper2D/Scripts/Grid.cs
@@ -3,73 +3,137 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace MineSweeper2D
-{ 
-public class Grid : MonoBehaviour
+{
+    public class Grid : MonoBehaviour
     {
         public GameObject tilePrefab;
-        public int width = 10;
-        public int height = 10;
-        public float spacing = .155f;
+        public static int width = 10;
+        public static int height = 10;
+        public float spacing = 0.155f;
+        public static Tile[,] tiles = new Tile[width, height];
 
-        private Tile[,] tiles;
-
-        //functionality for spawning tiles
-        Tile SpawnTile(Vector3 pos)
+        private float offset = 0.5f;
+        Ray ray;
+        // Use this for initialization
+        void Start()
         {
-            //clone tile prefab
-            GameObject clone = Instantiate(tilePrefab);
-            clone.transform.position = pos; //position tile
-            Tile currentTile = clone.GetComponent<Tile>(); //get tile component
-            return currentTile; //return it
+            // Generate tiles on startup
+            GenerateTiles();
         }
 
-        //spawns titles in a grid-like pattern
+        // functionality for spawning tiles
+        Tile SpawnTile(Vector3 pos)
+        {
+            // clone tile prefab
+            GameObject clone = Instantiate(tilePrefab);
+            clone.transform.position = pos; // position tile
+            Tile currentTile = clone.GetComponent<Tile>(); // get tile component
+            return currentTile; // return it
+        }
+        // spawn tiles in a grid-like pattern
         void GenerateTiles()
         {
-            //create new 2D array of size width x height
-            tiles = new Tile[width, height];
+            // create new 2D array of size width * height
+            //tiles = new Tile[width, height];
 
-            //loop through the entire tile llist
+            // loop through the entire tile list
             for (int x = 0; x < width; x++)
             {
                 for (int y = 0; y < height; y++)
                 {
-                    //store half size for later use
+                    // store half size for later use
                     Vector2 halfSize = new Vector2(width / 2, height / 2);
-
-                    //pivot tiles around Grid
-                    Vector2 pos = new Vector2(x - halfSize.x, y - halfSize.y);
-
-                    //apply spacing
+                    // Pivot tiles around grid
+                    Vector2 pos = new Vector2((x + offset) - halfSize.x, (y + offset) - halfSize.y); // applying offset to make it centre
+                    // apply spacing
                     pos *= spacing;
-
-                    //spawn the tile
+                    // spawn the tile
                     Tile tile = SpawnTile(pos);
-
-                    //attach newly spawned tile to
+                    // attach newly spawn tile to
                     tile.transform.SetParent(transform);
-
-                    //store it's array coordinates within itself for future reference
+                    // store it's array coordinates within itself for future reference
                     tile.x = x;
                     tile.y = y;
-
-                    //store tile in array at those coordinates
+                    // store tile in array at those coordinates
                     tiles[x, y] = tile;
-
                 }
             }
+        }
+        public int GetAdjacentMineCountAt(Tile t)
+        {
+            int count = 0;
+            // loop through all elements and have each axis go between -1 to 1
+            for (int x = -1; x <= 1; x++)
+            {
+                // calculate desired coordinates from ones attained
+                for (int y = -1; y <= 1; y++)
+                {
+                    int desiredX = t.x + x;
+                    int desiredY = t.y + y;
 
+                    if (desiredX >= 0 && desiredY >= 0 &&
+                        desiredX < width && desiredY < height)
+                    {
+                        Tile tile = tiles[desiredX, desiredY];
+                        if (tile.isMine)
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+            return count;
+            /*
+            int count = 0;
 
+            if (mineAt(t.x, t.y + 1)) ++count; // top
+            if (mineAt(t.x + 1, t.y + 1)) ++count; // top-right
+            if (mineAt(t.x + 1, t.y)) ++count; // right
+            if (mineAt(t.x + 1, t.y - 1)) ++count; // bottom-right
+            if (mineAt(t.x, t.y - 1)) ++count; // bottom
+            if (mineAt(t.x - 1, t.y - 1)) ++count; // bottom-left
+            if (mineAt(t.x - 1, t.y)) ++count; // left
+            if (mineAt(t.x - 1, t.y + 1)) ++count; // top-left
+
+            return count;
+            */
         }
 
-	    // Use this for initialization
-	    void Start () {
-            GenerateTiles();
+
+        //float AddNNumbers(float[] numbers)
+        //{
+        //    float result = 0;
+        //    for (int i = 0; i < numbers.Length; i++)
+        //    {
+        //        result += numbers[i];
+        //    }
+        //    return result;
+        //}
+
+        void FixedUpdate()
+        {
+            // IF mouse down (0)
+            if (Input.GetMouseButtonDown(0))
+            {
+                // LET ray = Camera main ScreenPointToRay(mouse Position)
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                // LET hit = Raycast(ray origin, ray direction)
+                RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+                // IF hit.collider != null
+                if (hit.collider != null)
+                {
+                    // LET tile = hit collider's Tile component
+                    Tile tile = hit.collider.GetComponent<Tile>();
+                    // IF tile != null
+                    if (tile != null)
+                    {
+                        // LET adjacentMines = GetAdjacentMineCountAt(tile)
+                        int adjacentMines = GetAdjacentMineCountAt(tile);
+                        // CALL tile.Reveal(adjacentMines)
+                        tile.Reveal(adjacentMines);
+                    }
+                }
+            }
         }
-	    
-	    // Update is called once per frame
-	    void Update () {
-	    	
-	    }
     }
 }
